ed8cb67f081290e7d85224e66bbef7d3c4ce6c47
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2020-11-24 18:04:38 +0700

    Добавьте файлы проекта.

---
+++ Lesson1WPF/Mail.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public static class Mail
+    {
+        public static List<string> GetMail(string text)
+        {
+            return new List<string>();
+        }
+
+        public static void SendMail(string login, string password, string address)
+        {
+            try
+            {
+                using (MailMessage mail = new MailMessage(login, address))
+                {
+                    mail.From = new MailAddress(login); // Адрес отправителя
+                    mail.To.Add(new MailAddress(address)); // Адрес получателя
+                    mail.Subject = "Заголовок";
+                    mail.Body = "Письмо........................";
+
+                    SmtpClient client = new SmtpClient();
+                    client.Host = "smtp.yandex.ru";
+                    client.Port = 587;
+                    client.Timeout = 5000;
+                    client.EnableSsl = true;
+                    client.Credentials = new NetworkCredential(login, password); // Ваши логин и пароль
+                    client.Send(mail);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Lesson1WPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginTb.Text;
+            var password = PasswordTb.Password;
+            var address = AddressTb.Text;
+
+            Mail.SendMail(login, password, address);
+
+        }
+    }
+}
