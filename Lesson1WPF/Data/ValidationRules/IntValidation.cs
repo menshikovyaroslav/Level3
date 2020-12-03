d5160fcfbf50012307d1cb591afdbe838bad2783
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2020-12-04 02:04:21 +0700

    Классы валидации, валидация №3

---
+++ Lesson1WPF/Data/ValidationRules/IntValidation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp1.Data.ValidationRules
+{
+    public class IntValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+
+            var result = 0;
+            var str = value.ToString();
+            Int32.TryParse(str, out result);
+
+            if (result != 0)
+            {
+                return new ValidationResult(true, "ok");
+            }
+            else
+            {
+                return new ValidationResult(false, "Это не число !");
+            }
+            
+        }
+    }
+}
---
+++ Lesson1WPF/Data/ValidationRules/MailValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp1.Data.ValidationRules
+{
+    public class MailValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var mail = value.ToString();
+            if (Regex.IsMatch(mail, @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$", RegexOptions.IgnoreCase))
+            {
+                return new ValidationResult(true, "ok");
+            }
+            else
+            {
+                return new ValidationResult(false, "Это не адрес электронной почты !");
+            }
+            
+        }
+    }
+}
