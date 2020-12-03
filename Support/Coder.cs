18cae09921e5adf674b78382628f51c671401580
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2020-12-03 23:40:50 +0700

    Добавлен класс кодировщик пароля

---
+++ Support/Coder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Support
+{
+    public static class Coder
+    {
+        public static string Encode(string pass)
+        {
+            var result = "";
+            foreach (var c in pass)
+            {
+                var ch = c;
+                ch--;
+                result += ch;
+            }
+            return result;
+        }
+
+        public static string Decode(string code)
+        {
+            var result = "";
+            foreach (var c in code)
+            {
+                var ch = c;
+                ch++;
+                result += ch;
+            }
+            return result;
+        }
+    }
+}
