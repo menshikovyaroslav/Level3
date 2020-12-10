8320a165c391c8878a8e255ac5952fe55fdaf51f
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2020-12-11 01:39:43 +0700

    await/async

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mainThread = Thread.CurrentThread;
+            Console.WriteLine($"Главный поток {mainThread.ManagedThreadId}");
+
+            Work();
+
+            Console.ReadLine();
+        }
+
+        static async void Work()
+        {
+            await SomeThingAsync();
+        }
+
+        static async Task SomeThingAsync()
+        {
+            var currThread = Thread.CurrentThread;
+            Console.WriteLine($"Перед async потоком {currThread.ManagedThreadId}");
+
+            await Task.Run(()=>
+            {
+                var mainThread = Thread.CurrentThread;
+                Console.WriteLine($"Из async потока {mainThread.ManagedThreadId}");
+            });
+
+            Console.WriteLine($"После async потока {currThread.ManagedThreadId}");
+        }
+
+    }
+}
---
+++ ConsoleApp13/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp13")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7d76a89d-79df-401e-b821-8651a8bd1bd6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
